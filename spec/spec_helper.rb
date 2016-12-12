bc6b2bc610ec4d3f4ab8b3876c84d3b6d6bbf57b
David Garcia <david.garcia@moviepilot.com>
2016-12-12 13:58:34 +0100

    Configure coverage correctly

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,3 @@
-$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-require 'bundler'
-Bundler.require
-
-require 'lounge_lizard'
-require 'rubygems'
 
 require 'simplecov'
 require 'coveralls'
@@ -12,7 +6,10 @@ SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
   SimpleCov::Formatter::HTMLFormatter,
   Coveralls::SimpleCov::Formatter
 ]
+
 SimpleCov.start { add_filter 'spec/' }
+
+require 'lounge_lizard'
 
 RSpec.configure do |config|
   config.filter_run focus: true
