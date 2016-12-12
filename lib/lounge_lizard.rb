5d173e8b9f65283e0ce220f64c4288127a28ead4
David Garcia <david.garcia@moviepilot.com>
2016-12-12 13:21:44 +0100

    Add binding

--- lib/lounge_lizard.rb
+++ lib/lounge_lizard.rb
@@ -1,5 +1,18 @@
-require "lounge_lizard/version"
+require 'lounge_lizard/version'
+require 'ffi'
+require 'json'
+require 'lounge_lizard/binding'
 
+# Convenience methods, so you don't have to access the bindings directly
 module LoungeLizard
-  # Your code goes here...
+  def self.parse(mson)
+    parse_result = FFI::MemoryPointer.new :pointer
+    Binding.drafter_parse_blueprint_to(mson, parse_result, Binding::DrafterOptions.as_json)
+    parse_result = parse_result.get_pointer(0)
+    result_string = parse_result.read_string unless parse_result.null?
+    return {} if result_string.to_s.empty?
+    JSON.parse(result_string)
+  ensure
+    parse_result.free
+  end
 end
--- spec/lounge_lizard_spec.rb
+++ spec/lounge_lizard_spec.rb
@@ -1,11 +1,27 @@
-require "spec_helper"
+require 'spec_helper'
 
 describe LoungeLizard do
-  it "has a version number" do
+
+  it 'has a version number' do
     expect(LoungeLizard::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe '.parse' do
+
+    it 'can parse a mson string into a hash representing an ast' do
+      mson_ast = LoungeLizard.parse("# XXXX\ndescription for it")
+      expect('XXXX').to eq mson_ast['content'].first['meta']['title']
+      expect('description for it').to eq  mson_ast['content'].first['content'].first['content']
+    end
+
+    context 'with invalid msons' do
+      it 'does not raise an error when the mson is invalid' do
+        expect { LoungeLizard.parse('Thi;lhBABBLINGad\'') }.not_to raise_error
+      end
+
+      it 'does not raise an error when the mson is nil' do
+        expect { LoungeLizard.parse(nil) }.not_to raise_error
+      end
+    end
   end
 end
